4ad9824cad00992dc2febdb8914b55c7da1e8f3b
Abhishek Kumar <abhishek.kr0499@gmail.com>
2026-06-14 23:16:21 +0530

    initial project setup

---
+++ config/settinigs.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    app_name: str
+
+    database_url: str
+
+    redis_host: str
+    redis_port: int
+
+    jwt_secret: str
+    jwt_algorithm: str
+
+    upstox_api_key: str
+    upstox_api_secret: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
